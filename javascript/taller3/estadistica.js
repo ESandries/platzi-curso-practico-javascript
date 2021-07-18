ba604ce5fd24399de4916bf2ff1b28ea47d4ad1c
dev-esandries <emanuelsandries27@gmail.com>
2021-07-17 23:43:11 -0600

    Se agrego la funcionalidad de calcular la mediana

--- javascript/taller3/estadistica.js
+++ javascript/taller3/estadistica.js
@@ -10,6 +10,39 @@ function formulaMediaAritmetica(lista){
     return promediolista
 } 
 
+function formulaMediana(list){
+    let lista = list;
+    const mitadlista = parseInt(lista.length / 2);
+    let mediana;
+
+    function esPar(numerito){
+        if(numerito % 2 == 0){
+            return true;
+        } else {
+            return false;
+        }   
+    }
+
+    if(esPar(lista.length)){
+        const elemnto1 = lista[mitadlista];
+        const elemnto2 = lista[mitadlista + 1];
+        const elemnto1y2 = formulaMediaAritmetica([
+            elemnto1,
+            elemnto2,
+        ])
+
+        mediana =  elemnto1y2;
+
+    } else{
+        mediana = lista[mitadlista];
+    }
+
+    console.log(mediana);
+
+    return mediana;
+}
+
+
 function calcularMediaAritmetica(){
     let resultSelect = document.getElementById("resultEstadistica");
 
@@ -22,3 +55,17 @@ function calcularMediaAritmetica(){
 
     resultSelect.innerText = "El promedio de los datos ingresados es: " +  promedio;
 }
+
+function calcularMediana(){
+    let resultSelect = document.getElementById("resultEstadistica");
+
+    let inputSelect = document.getElementById("datos");
+    let value = inputSelect.value;
+    console.log(value)
+    let valueArray = Array.from(value.split(','),Number)
+    console.log(valueArray)
+    let mediana = formulaMediana(valueArray);
+
+    resultSelect.innerText = "La mediana de los datos ingresados es: " +  mediana;
+}
+
